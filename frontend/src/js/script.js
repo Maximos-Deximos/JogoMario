4a5d6b5cbb0e5736e9e9d66fa7eae57192a2b2bd
Maximos-Deximos <mnlbzrlh@proton.me>
2026-09-17 15:48:09 -0300

    add: graficos e logica de funcionamento do jogo.

---
+++ frontend/src/js/script.js
@@ -0,0 +1,29 @@
+const naomario = document.querySelector('.naomario');
+const tubo = document.querySelector('.tubo')
+
+const pulo = () => {
+    naomario.classList.add('pulo');
+
+    setTimeout(() => {
+    
+        naomario.classList.remove('pulo');
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const tuboPosicao = tubo.offsetLeft;
+    console.log(tuboPosicao)
+    const naomarioPosicao = +window.getComputedStyle(naomario).bottom.replace('px', '');
+    console.log(naomarioPosicao)
+
+    if (tuboPosicao <= 120 && naomarioPosicao < 75 && tuboPosicao > 0) {
+        tubo.style.animation = 'none';
+        tubo.style.animation `$(tuboPosicao)px;`
+    }
+
+}, 10)
+
+
+document.addEventListener("keydown", pulo);
+
